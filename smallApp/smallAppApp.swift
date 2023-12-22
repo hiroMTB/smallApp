c598ec9cd6ba494143225d1afcc192febd329c1c
matoba hiroshi <matobahiroshi@gmail.com>
2023-12-22 15:30:11 +0100

    init

---
+++ smallApp/smallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  smallAppApp.swift
+//  smallApp
+//
+//  Created by Hiroshi Matoba on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct smallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
